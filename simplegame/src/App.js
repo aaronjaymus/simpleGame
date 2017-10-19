eab5ea72cf263e62ed00b26788809159941f6a2a
aaronjaymus <aaronjaymus@gmail.com>
2017-10-19 07:59:57 -0400

    Working on login and logout. Trying to figure out how to add user to db after checking if user is in the db first.

--- simplegame/src/App.js
+++ simplegame/src/App.js
@@ -11,11 +11,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      user: null,
+      database: null,
+      usersRef: null
     }
 
     this.login = this.login.bind(this); 
-    this.logout = this.logout.bind(this);    
+    this.logout = this.logout.bind(this);
+    this.addUser = this.addUser.bind(this);    
   }
   
   logout() {
@@ -31,13 +34,33 @@ class App extends Component {
     auth.signInWithPopup(provider)
       .then((result) => {
         const user = result.user;
+        console.log("User name: " + user.displayName);
+        console.log("User email: " + user.email);
+        console.log("User id: " + user.uid);
+        console.log(JSON.stringify(user));
+        this.addUser(user);
         this.setState({
           user
         });
       });
   }
+  //Check if user is in DB, and if not, add to DB
+  addUser() {
+    var usersRef = this.state.database.ref("users");
+    console.log(usersRef);
+    this.setState({
+      usersRef
+    });
+  }
 
   componentDidMount() {
+    //grab database and set state
+    var database = fire.database();
+    console.log(database);
+    this.setState({
+      database
+    });
+    //check if user was already logged in to prevent logout on refresh
     auth.onAuthStateChanged((user) => {
       if(user) {
         this.setState({ user });
@@ -55,14 +78,27 @@ class App extends Component {
             :
             <button className="btn" onClick={this.login}>Log In</button>              
           }
+          {this.state.user ?
+            <div>
+              <div className='user-profile'>
+                <img src={this.state.user.photoURL} />
+              </div>
+            </div> 
+            :
+            <div className='wrapper'>
+              <p>
+                You must be logged in to play
+              </p>
+            </div>
+          }
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           <p><Link to="/">Home</Link></p>
           <p><Link to="/rps">Rock Paper Scissors</Link></p>
           <p><Link to="/ttt">Tic Tac Toe</Link></p>
           <p><Link to="/war">War</Link></p>
           {this.props.children}
-        </p>
+        </div>
       </div>
     );
   }
